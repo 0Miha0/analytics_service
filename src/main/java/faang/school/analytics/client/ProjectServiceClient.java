f4dcbc6b1e9ab03ef5f31fad4787ca1cb8cf57e5
Maria Sahno <sahnomyu@m2.ru>
2024-12-23 01:32:12 +0500

    clean up

--- src/main/java/faang/school/analytics/client/ProjectServiceClient.java
+++ src/main/java/faang/school/analytics/client/ProjectServiceClient.java
@@ -1,6 +1,5 @@
 package faang.school.analytics.client;
 
-
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(name = "project-service", url = "${project-service.host}:${project-service.port}")
